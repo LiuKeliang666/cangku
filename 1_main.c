0b28663fc81cd7bf9d3bab78f58045cd7f3cba36
LiuKeliang666 <1443785738@qq.com>
2025-10-25 00:27:33 +0800

    提交第一题

--- 1_main.c
+++ 1_main.c
@@ -1,28 +1,17 @@
 //202511716105
 //1443785738@qq.com
 //刘科良
-#include <stdio.h>
+include <stdio.h>
 
-int main()
-{   
-    int score;
+int main() {
+    float celsius, fahrenheit;
     
-    printf("请输入学生成绩：");
-
-    scanf("%d", &score);
-
-    if (score >= 90 && score <= 100)       {
-        printf("A\n");
-     }else if (score >= 80 && score <= 89) {
-        printf("B\n");  
-     }else if (score >= 70 && score <= 79) {
-        printf("C\n");
-     }else if (score >= 60 && score <= 69) {
-        printf("D\n");
-     }else if (score >= 0 && score <= 60)  {
-        printf("E\n");
-     }else                                 {
-        printf("输入成绩无效。\n");
-     }
-      return 0;
+    printf("请输入摄氏温度：");
+    scanf("%f", &celsius);
+    
+    fahrenheit = celsius * 9 / 5 + 32;
+    
+    printf("华氏温度为：%.1f\n", fahrenheit);
+    
+    return 0;
 }
