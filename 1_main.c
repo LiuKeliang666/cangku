b2c2d76b4d32eb10b935e536cbe12921090815d0
LiuKeliang666 <1443785738@qq.com>
2025-10-24 22:32:01 +0800

    提交第一题
    Replace Hello World program with Celsius to Fahrenheit converter.

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,19 @@
-//
-//
-//
+//202511716105
+//1443785738@qq.com
+//刘科良
 #include <stdio.h>
+
 int main()
-{
-    printf("Hello World");
-    return 0;
-}
+{   
+    float celsius, fahrenheit;
+
+    printf("请用户输入摄氏温度");
+
+    scanf("%f",&celsius);
+
+    fahrenheit = celsius * 9.0 / 5.0 +32.0;
+
+    printf("华氏温度为: %.1f/n", fahrenheit);
+
+    return 0;   
+}   
