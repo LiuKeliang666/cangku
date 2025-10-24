be32d238dcfc40512c71e634c3691eeeeffebbbd
LiuKeliang666 <1443785738@qq.com>
2025-10-25 00:01:42 +0800

    提交第二题

---
+++ 2_main.c
@@ -0,0 +1,28 @@
+//202511716105
+//1443785738@qq.com
+//刘科良
+#include <stdio.h>
+
+int main()
+{   
+    int score;
+    
+    printf("请输入学生成绩：");
+
+    scanf("%d", &score);
+
+    if (score >= 90 && score <= 100)       {
+        printf("A\n");
+     }else if (score >= 80 && score <= 89) {
+        printf("B\n");  
+     }else if (score >= 70 && score <= 79) {
+        printf("C\n");
+     }else if (score >= 60 && score <= 69) {
+        printf("D\n");
+     }else if (score >= 0 && score <= 60)  {
+        printf("E\n");
+     }else                                 {
+        printf("输入成绩无效。\n");
+     }
+      return 0;
+}
