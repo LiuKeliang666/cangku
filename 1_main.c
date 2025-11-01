a43559e73cfa0d57bfee029f32e028abc09c14fb
LiuKeliang666 <1443785738@qq.com>
2025-11-01 13:19:38 +0800

    提交第一题
    Added prime number check and input validation for n.

--- 1_main.c
+++ 1_main.c
@@ -1,17 +1,37 @@
 //202511716105
 //1443785738@qq.com
 //刘科良
-include <stdio.h>
+#include <stdio.h>
 
-int main() {
-    float celsius, fahrenheit;
+int main() 
+    {int n;
     
-    printf("请输入摄氏温度：");
-    scanf("%f", &celsius);
+    scanf("%d", &n);
     
-    fahrenheit = celsius * 9 / 5 + 32;
+    if (n <= 0 || n >= 50) {
+        printf("输入无效，请输入小于50的正整数\n");
+        return 0;
+    }
     
-    printf("华氏温度为：%.1f\n", fahrenheit);
+    if (n == 1) {
+        printf("密钥不安全，请重新输入");
+        return 0;
+    }
+    
+    int isPrime = 1; // 假设是质数
+    
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            isPrime = 0; // 找到因子，不是质数
+            break;
+        }
+    }
+    
+    if (isPrime) {
+        printf("密钥安全，密码设置成功");
+    } else {
+        printf("密钥不安全，请重新输入");
+    }
     
     return 0;
 }
