c73f3d240ffe9ee4f2d26f3c8202ef9a2d4e5308
LiuKeliang666 <1443785738@qq.com>
2025-11-01 13:52:29 +0800

    提交第二题

--- 2_main.c
+++ 2_main.c
@@ -2,28 +2,26 @@
 //1443785738@qq.com
 //刘科良
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-    int count = 0; 
-    // 遍历100到999的所有数
-    for (int num = 100; num <= 999; num++) {
-        int hundreds = num / 100;        // 百位数
-        int tens = (num / 10) % 10;      // 十位数
-        int units = num % 10;            // 个位数
-        
-        // 计算各位数字的立方和
-        int sum = pow(hundreds, 3) + pow(tens, 3) + pow(units, 3);
+    int num, hundreds, tens, units;
+    int first_output = 1;
+    
+    for (num = 100; num <= 999; num++) {
+        // 拆分各位数字
+        hundreds = num / 100;
+        tens = (num / 10) % 10;
+        units = num % 10;
         
         // 判断是否为水仙花数
-        if (sum == num) {
-            if (count > 0) {
-                printf(" "); // 在数字前加空格（第一个数字前不加）
+        if (hundreds*hundreds*hundreds + tens*tens*tens + units*units*units == num) {
+            if (first_output) {
+                printf("%d", num);
+                first_output = 0;
+            } else {
+                printf(" %d", num);
             }
-            printf("%d", num);
-            count++;
         }
     }
-    
     return 0;
 }
