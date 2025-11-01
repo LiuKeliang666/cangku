428812d2278670f032f20b5a71fd0143d5a86a01
LiuKeliang666 <1443785738@qq.com>
2025-11-01 13:33:06 +0800

    提交第三题

--- 3_main.c
+++ 3_main.c
@@ -4,21 +4,35 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
-    char op;
+    int n;
     
-    scanf("%d %d %c", &a, &b, &op);
+    scanf("%d", &n);
     
-    if (op == '+') {
-        printf("%d", a + b);
-    } else if (op == '-') {
-        printf("%d", a - b);
-    } else if (op == '*') {
-        printf("%d", a * b);
-    } else if (op == '/') {
-        if (b != 0) {
-            printf("%d", a / b);
+    if (n <= 0 || n >= 50) {
+        printf("输入无效，请输入小于50的正整数\n");
+        return 0;
+    }
+    
+    if (n == 1) {
+        printf("密钥不安全，请重新输入");
+        return 0;
+    }
+    
+    int isPrime = 1; // 假设是质数
+    int i = 2;
+    
+    while (i * i <= n) {
+        if (n % i == 0) {
+            isPrime = 0; // 找到因子，不是质数
+            break;
         }
+        i++;
+    }
+    
+    if (isPrime) {
+        printf("密钥安全，密码设置成功");
+    } else {
+        printf("密钥不安全，请重新输入");
     }
     
     return 0;
