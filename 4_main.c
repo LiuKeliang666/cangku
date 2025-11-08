1178bd292ad7fa898e2d0e12f4ceb07f57919d0f
LiuKeliang666 <1443785738@qq.com>
2025-11-08 16:36:28 +0800

    提交第四题
    Refactor main function to calculate sum of squares using a new power function.

--- 4_main.c
+++ 4_main.c
@@ -3,33 +3,19 @@
 //刘科良
 #include <stdio.h>
 
-int main() {
-    int num = 100;
-    int count = 0; 
-    
-    while (num <= 999) {
-        int original = num;
-        int sum = 0;
-        
-        // 使用while循环计算各位数字的立方和
-        int temp = num;
-        while (temp > 0) {
-            int digit = temp % 10;
-            sum += digit * digit * digit;
-            temp = temp / 10;
-        }
-        
-        // 判断是否为水仙花数
-        if (sum == num) {
-            if (count > 0) {
-                printf(" "); // 在数字前加空格（第一个数字前不加）
-            }
-            printf("%d", num);
-            count++;
-        }
-        
-        num++;
+int power(int a, int b) {
+    int result = 1;
+    for (int i = 0; i < b; i++) {
+        result *= a;
     }
-    
+    return result;
+}
+
+int main() {
+    int sum = 0;
+    for (int i = 1; i <= 5; i++) {
+        sum += power(i, 2);
+    }
+    printf("%d\n", sum); 
     return 0;
 }
