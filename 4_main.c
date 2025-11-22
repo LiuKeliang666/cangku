2b3674203102488ab97b70c6417673f705e1537a
LiuKeliang666 <1443785738@qq.com>
2025-11-22 13:43:43 +0800

    提交第四题

--- 4_main.c
+++ 4_main.c
@@ -2,20 +2,20 @@
 //1443785738@qq.com
 //刘科良
 #include <stdio.h>
-
-int power(int a, int b) {
-    int result = 1;
-    for (int i = 0; i < b; i++) {
-        result *= a;
+// 指针操作数组元素自增
+void increment(int *arr) {
+    for (int i = 0; i < 5; i++) {
+        *(arr + i) += 1;
     }
-    return result;
 }
-
 int main() {
-    int sum = 0;
-    for (int i = 1; i <= 5; i++) {
-        sum += power(i, 2);
+    int arr[5];
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
     }
-    printf("%d\n", sum); 
+    increment(arr);  // 数组名作为指针传入
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
     return 0;
 }
