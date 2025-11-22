16ee7cf50431fc359268fc4c9ddcf588f3130fc4
LiuKeliang666 <1443785738@qq.com>
2025-11-22 13:44:44 +0800

    提交第五题
    Added a function to shift array elements and updated main to use it.

--- 5_main.c
+++ 5_main.c
@@ -2,14 +2,22 @@
 //1443785738@qq.com
 //刘科良
 #include <stdio.h>
-
-int sum_arithmetic(int a, int b, int c) {
-    int count = (b - a) / c + 1;
-    return count * (a + b) / 2;
+// 指针实现数组后移
+void shift(int *ptr_arr, int len) {
+    // 从最后一个元素向前遍历，将前一个元素的值赋给当前元素
+    for (int i = len - 1; i > 0; i--) {
+        *(ptr_arr + i) = *(ptr_arr + i - 1);
+    }
+    *ptr_arr = 0;   // 首元素补0
 }
-
 int main() {
-    int result = sum_arithmetic(1, 100, 1);
-    printf("%d\n", result);
+    int arr[5];
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+    shift(arr, 5);
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
     return 0;
 }
