af313cdc38a7261b620c53df44db087bb7408ef2
LiuKeliang666 <1443785738@qq.com>
2025-11-22 13:40:46 +0800

    提交第二题

--- 2_main.c
+++ 2_main.c
@@ -2,22 +2,20 @@
 //1443785738@qq.com
 //刘科良
 #include <stdio.h>
-
 int main() {
-    int matrix[3][3];
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            scanf("%d", &matrix[i][j]);
-        }
+    int arr[5];
+    // 输入数组元素
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
     }
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%d", matrix[j][i]);
-            if (j < 2) {
-                printf(" ");
-            }
-        }
-        printf("\n");
+    int *p = arr;   // 数组名arr等价于数组首元素地址
+    // 指针遍历数组并乘以2
+    for (int i = 0; i < 5; i++) {
+        *(p + i) *= 2;
+    }
+    // 输出结果
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
     }
     return 0;
 }
