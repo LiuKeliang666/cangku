ec45af28743a2f5b0eabdbc0821969cf234acdbb
LiuKeliang666 <1443785738@qq.com>
2025-11-22 13:48:57 +0800

    提交第七题
    Added bubble sort function to sort an array and updated main to use it.

--- 7_main.c
+++ 7_main.c
@@ -2,27 +2,27 @@
 //1443785738@qq.com
 //刘科良
 #include <stdio.h>
-
-void reverse_array(int arr[], int len) {
-    for (int i = 0; i < len / 2; i++) {
-        int temp = arr[i];
-        arr[i] = arr[len - 1 - i];
-        arr[len - 1 - i] = temp;
-    }
-}
-
-int main() {
-    int arr[5];
-    for (int i = 0; i < 5; i++) {
-        scanf("%d", &arr[i]);
-    }
-    reverse_array(arr, 5);
-    for (int i = 0; i < 5; i++) {
-        printf("%d", arr[i]);
-        if (i < 4) {
-            printf(" ");
+// 指针实现冒泡排序
+void bubbleSort(int *arr, int len) {
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = 0; j < len - 1 - i; j++) {
+            // 比较相邻元素，若逆序则交换
+            if (*(arr + j) > *(arr + j + 1)) {
+                int temp = *(arr + j);
+                *(arr + j) = *(arr + j + 1);
+                *(arr + j + 1) = temp;
+            }
         }
     }
-    printf("\n");
+}
+int main() {
+    int arr[10];
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
+    }
+    bubbleSort(arr, 10);
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", arr[i]);
+    }
     return 0;
 }
