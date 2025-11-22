0193c930c192ba9bf1a085f0e2809c102bf03939
LiuKeliang666 <1443785738@qq.com>
2025-11-22 13:39:32 +0800

    提交第一题
    Removed 2D array input/output and added pointer manipulation.

--- 1_main.c
+++ 1_main.c
@@ -2,14 +2,11 @@
 //1443785738@qq.com
 //刘科良
 #include <stdio.h>
-
 int main() {
-    int arr[3][3];
-    for (int i = 0; i < 3; i++) {
-        scanf("%d %d %d", &arr[i][0], &arr[i][1], &arr[i][2]);
-    }
-    for (int i = 0; i < 3; i++) {
-        printf("%d %d %d\n", arr[i][0], arr[i][1], arr[i][2]);
-    }
+    int a;
+    scanf("%d", &a);
+    int *p = &a;   // 指针p指向a
+    *p += 10;      // 通过指针修改a的值
+    printf("%d,%d\n", a, *p);
     return 0;
 }
